2fc7e0bca80092704fd21f1c9ba786a353369eb9
codewith-PS <parikshits563@gmail.com>
2026-02-12 00:28:36 +0530

    fixed duplicate import

--- src/certificate.jsx
+++ src/certificate.jsx
@@ -8,6 +8,9 @@ export default function certificate() {
             <section id="Certificate" className=" h-screen pt-40 gap-10 flex flex-wrap justify-center items-center text-white relative" style={{ backgroundColor: '#170E26', height: 'max-content' }}>
                 <p className='font-semibold absolute top-20 text-4xl bg-gradient-to-r from-purple-500 via-fuchsia-400 to-fuchsia-700 text-transparent bg-clip-text' id='fset' style={{ fontFamily: 'serif' }}>Certificates</p>
                 <div data-aos="zoom-in" className="flex justify-center text-center md:text-left w-full md:w-[500px] h-90 p-6 relative">
+                    <img src='certificatenew.jpeg' />
+                </div>
+                <div data-aos="zoom-in" className="flex justify-center text-center md:text-left w-full md:w-[500px] h-90 p-6 relative">
                     <img src='certificate2.jpg' />
                 </div>
                 <div data-aos="zoom-in" className='flex justify-center text-center md:text-left w-full md:w-[500px] h-90 p-5 relative'>
